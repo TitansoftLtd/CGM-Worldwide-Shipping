a24afb2fa681d39c4279e0c906315e30da75eae1
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2026-06-09 19:09:01 +0000

    feat: Implement autoname functionality for Permit Type and enforce required Permit Name

--- cgm_shipping/cgm_worldwide_shipping/doctype/permit_type/permit_type.py
+++ cgm_shipping/cgm_worldwide_shipping/doctype/permit_type/permit_type.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2026, Titansoft Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class PermitType(Document):
-	pass
+	def autoname(self):
+		# Name by the Permit Name (naming_rule: "By script").
+		if not self.permit_name:
+			frappe.throw(frappe._("Permit Name is required"))
+		self.name = self.permit_name.strip()
