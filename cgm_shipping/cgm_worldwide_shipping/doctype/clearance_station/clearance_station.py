d26b22bf1acf1d6980ed9275b03836d2d446e870
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2026-06-09 08:33:09 +0000

    feat: Implement autoname method for Clearance Station to set name based on cfs_name field

--- cgm_shipping/cgm_worldwide_shipping/doctype/clearance_station/clearance_station.py
+++ cgm_shipping/cgm_worldwide_shipping/doctype/clearance_station/clearance_station.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2026, Titansoft Limited and contributors
 # For license information, please see license.txt
 
-import frappe
 from frappe.model.document import Document
 
 
 class ClearanceStation(Document):
-	pass
+	def autoname(self):
+		self.name = (self.cfs_name or "").strip()
